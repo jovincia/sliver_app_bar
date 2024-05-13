9cbbeae21d9755675cf41f45ba0e3ac847fab5f0
Jovincia <assogbadanielle15@gmail.com>
2024-05-13 23:59:53 +0100

    new add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_app_bar/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MainColors.brownCoffeeMilk,
+    );
   }
 }
