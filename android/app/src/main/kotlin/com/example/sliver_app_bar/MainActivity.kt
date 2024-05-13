c8f4766cfe39700170732b2c60e32b379f4e134e
Jovincia <assogbadanielle15@gmail.com>
2024-05-13 23:56:38 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/sliver_app_bar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sliver_app_bar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainColors {
+  static Color brown = const Color(0xFF8B572A);
+  static Color brownNude = const Color(0xFFD2B48C);
+  static Color brownCoffeeMilk = const Color(0xFFA67B5B);
+  static Color brown1 = Color.fromARGB(255, 221, 155, 98);
+  static Color brown2 = Color.fromARGB(255, 241, 172, 111);
+  static Color? white = Colors.white;
+}
