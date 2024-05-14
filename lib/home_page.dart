88f0bdcef1a48ce11cfde6ded7c61f8f5a082afe
Jovincia <assogbadanielle15@gmail.com>
2024-05-14 23:47:39 +0100

    new add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sliver_app_bar/colors.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,28 +9,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MainColors.brownNude,
       body: CustomScrollView(
         slivers: [
           //sliver app bar
           SliverAppBar(
             backgroundColor: MainColors.brownCoffeeMilk,
-            leading: Icon(Icons.menu),
-            title: Text("M A D E B Y D A N I ♥"),
+            leading: Icon(
+              Icons.menu,
+              color: MainColors.white,
+            ),
+            title: Text(
+              "M A D E B Y D A N I ♥",
+              style: GoogleFonts.rajdhani(
+                color: MainColors.white,
+                fontSize:30,
+                ),
+            ),
             expandedHeight: 200,
           ),
           //sliver Items
           SliverToBoxAdapter(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(70.0),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(20),
                 child: Container(
-                  height: 300,
-                  color: MainColors.brownNude,
+                  height: 250,
+                  color: MainColors.brownCoffeeMilk,
+                ),
+              ),
+            ),
+          ),
+                    SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(70.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height:250,
+                  //width: 400,
+                  color: MainColors.brownCoffeeMilk,
+                ),
+              ),
+            ),
+          ),
+                    SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(70.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 250,
+                  color: MainColors.brownCoffeeMilk,
                 ),
               ),
             ),
           )
+
         ],
       ),
     );
