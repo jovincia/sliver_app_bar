564b9c9fce62080891afdce5f23947aba823aa50
Jovincia <assogbadanielle15@gmail.com>
2024-12-09 00:04:04 +0100

    new updates

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,7 +4,7 @@ class MainColors {
   static Color brown = const Color(0xFF8B572A);
   static Color brownNude = const Color(0xFFD2B48C);
   static Color brownCoffeeMilk = const Color(0xFFA67B5B);
-  static Color brown1 = Color.fromARGB(255, 221, 155, 98);
-  static Color brown2 = Color.fromARGB(255, 241, 172, 111);
+  static Color brown1 = const Color.fromARGB(255, 221, 155, 98);
+  static Color brown2 = const Color.fromARGB(255, 241, 172, 111);
   static Color? white = Colors.white;
 }
